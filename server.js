021ca7bfa58201e57306536bac20db3c50aa9525
Ajin <ajin.varghese@contentstack.com>
2021-01-24 18:18:54 +0530

    Resolved Cors issue

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema");
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const app = express();
-
+app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({
