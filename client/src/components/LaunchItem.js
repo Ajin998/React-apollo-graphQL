93e7966444e6cb39d0bd3f6d5656c4f02ce7b90e
Ajin Varghese <ajin.varghese@contentstack.com>
2021-01-24 22:47:37 +0530

    Added classnames npm package
    For distingusinghing success and failure mission

--- client/src/components/LaunchItem.js
+++ client/src/components/LaunchItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import classnames from "classnames";
 function LaunchItem({
   launch: { flight_number, mission_name, launch_date_local, launch_success },
 }) {
@@ -7,8 +7,20 @@ function LaunchItem({
     <div className="card card-body mb-3">
       <div className="row">
         <div className="col-md-9">
-          <h5>Mission:{mission_name}</h5>
-          <p>Date:{launch_date_local}</p>
+          <h5>
+            Mission:{" "}
+            <span
+              className={classnames({
+                "text-success": launch_success,
+                "text-danger": !launch_success,
+              })}
+            >
+              {mission_name}
+            </span>
+          </h5>
+          <p>
+            Date: <span>{launch_date_local}</span>
+          </p>
         </div>
         <div className="col-md-3">
           <button className="btn btn-secondary">Launch Details</button>
